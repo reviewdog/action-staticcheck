8a7ad76b01501c940bc5ba46c9b85371781b6126
haya14busa <haya14busa@gmail.com>
2020-05-10 10:47:56 +0000

    debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,8 +12,12 @@ echo "RUNNER_TOOL_CACHE=${RUNNER_TOOL_CACHE}"
 if [ -n "${RUNNER_TOOL_CACHE}" ]; then
   # https://github.com/actions/toolkit/blob/83dd3ef0f1e5bc93c5ab7072e1edf1715a01ba9d/packages/tool-cache/src/tool-cache.ts#L547
   mkdir -p "${RUNNER_TOOL_CACHE}/action-staticcheck"
-  ls "${RUNNER_TOOL_CACHE}/action-staticcheck" # debug
   export XDG_CACHE_HOME="${RUNNER_TOOL_CACHE}/action-staticcheck"
+  echo "XDG_CACHE_HOME=${XDG_CACHE_HOME}"
+  echo "ls ${RUNNER_TOOL_CACHE}/action-staticcheck"
+  ls "${RUNNER_TOOL_CACHE}/action-staticcheck" # debug
+  echo "ls "${XDG_CACHE_HOME}""
+  ls "${XDG_CACHE_HOME}" # debug
 fi
 
 staticcheck ${INPUT_STATICCHECK_FLAGS} -f=json ${INPUT_TARGET:-.} \
