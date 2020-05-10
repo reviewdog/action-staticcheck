1ebb26d73df697d0091bcb9e83dfb543dd95be2a
haya14busa <haya14busa@gmail.com>
2020-05-10 10:39:47 +0000

    Support cache

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,13 @@ fi
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
+if [ -n "${RUNNER_TOOL_CACHE}" ]; then
+  # https://github.com/actions/toolkit/blob/83dd3ef0f1e5bc93c5ab7072e1edf1715a01ba9d/packages/tool-cache/src/tool-cache.ts#L547
+  mkdir -p "${RUNNER_TOOL_CACHE}/action-staticcheck"
+  ls "${RUNNER_TOOL_CACHE}/action-staticcheck" # debug
+  export XDG_CACHE_HOME="${RUNNER_TOOL_CACHE}/action-staticcheck"
+fi
+
 staticcheck ${INPUT_STATICCHECK_FLAGS} -f=json ${INPUT_TARGET:-.} \
   | tmpl -f=jsonl /json.tmpl \
   | reviewdog \
